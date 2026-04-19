7050c819ff23d084f0fd9f7cf3644cc7928c6ded
Andrea Zhang <andrea02polimi@gmail.com>
2026-04-19 18:42:46 +0200

    working version v3 - exp_05, exp_06

--- scripts/train-eval.sh
+++ scripts/train-eval.sh
@@ -32,31 +32,7 @@ run_exp() {
         "$@"
 }
 
-run_exp exp_03_christiano-rew_model-pref-SAC-seg_len_full_ep \
-    "${COMMON[@]}" \
-    algo=christiano \
-    algo/agent=SAC \
-    algo.train.kwargs.total_timesteps=1000000 \
-    algo.train.kwargs.n_initial_queries=200 \
-    algo.train.kwargs.n_queries_per_iter=10 \
-    algo.train.kwargs.reward_model_train_steps=200 \
-    algo.train.kwargs.reward_model_batch_size=64 \
-    algo.kwargs.reward_model_n_networks=2 \
-    algo.kwargs.reward_model_hidden_size=256 \
-    algo.kwargs.reward_model_lr=3e-4 \
-    algo.kwargs.reward_model_l2=1e-4 \
-    algo.kwargs.segment_length=None \
-    algo.kwargs.episode_length_estimate=1 \
-    algo.kwargs.preference_dataset_max_size=3000 \
-    algo.kwargs.query_schedule=constant \
-    algo.agent.kwargs.learning_rate=3e-4 \
-    algo.agent.kwargs.buffer_size=100000 \
-    algo.agent.kwargs.learning_starts=100 \
-    algo.agent.kwargs.batch_size=256 \
-    algo.agent.kwargs.gradient_steps=1000 \
-    algo.agent.kwargs.train_freq=8000
-
-run_exp exp_04_christiano-rew_model-pref-PPO-seg_len_full_ep \
+run_exp exp_05_christiano-rew_model-pref-SAC-seg_len_10 \
     "${COMMON[@]}" \
     algo=christiano \
     algo/agent=PPO \
@@ -69,11 +45,35 @@ run_exp exp_04_christiano-rew_model-pref-PPO-seg_len_full_ep \
     algo.kwargs.reward_model_hidden_size=256 \
     algo.kwargs.reward_model_lr=3e-4 \
     algo.kwargs.reward_model_l2=1e-4 \
-    algo.kwargs.segment_length=None \
-    algo.kwargs.episode_length_estimate=1 \
+    algo.kwargs.segment_length=10 \
+    algo.kwargs.episode_length_estimate=None \
     algo.kwargs.preference_dataset_max_size=3000 \
     algo.kwargs.query_schedule=constant \
     algo.agent.kwargs.learning_rate=3e-4 \
     algo.agent.kwargs.batch_size=64 \
     algo.agent.kwargs.n_epochs=10 \
     algo.agent.kwargs.n_steps=1000
+
+run_exp exp_06_christiano-rew_model-pref-SAC-seg_len_10 \
+    "${COMMON[@]}" \
+    algo=christiano \
+    algo/agent=SAC \
+    algo.train.kwargs.total_timesteps=1000000 \
+    algo.train.kwargs.n_initial_queries=200 \
+    algo.train.kwargs.n_queries_per_iter=10 \
+    algo.train.kwargs.reward_model_train_steps=200 \
+    algo.train.kwargs.reward_model_batch_size=64 \
+    algo.kwargs.reward_model_n_networks=2 \
+    algo.kwargs.reward_model_hidden_size=256 \
+    algo.kwargs.reward_model_lr=3e-4 \
+    algo.kwargs.reward_model_l2=1e-4 \
+    algo.kwargs.segment_length=10 \
+    algo.kwargs.episode_length_estimate=None \
+    algo.kwargs.preference_dataset_max_size=3000 \
+    algo.kwargs.query_schedule=constant \
+    algo.agent.kwargs.learning_rate=3e-4 \
+    algo.agent.kwargs.buffer_size=100000 \
+    algo.agent.kwargs.learning_starts=100 \
+    algo.agent.kwargs.batch_size=256 \
+    algo.agent.kwargs.gradient_steps=1000 \
+    algo.agent.kwargs.train_freq=8000
